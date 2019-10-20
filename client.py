3845338327c4db599a57a8a3a65ed2eb6c44307b
wjojarth123 <wjojarth@gmail.com>
2019-10-20 13:39:44 -0700

    no commit message

---
+++ client.py
@@ -0,0 +1,40 @@
+import socket
+import atexit
+import threading
+from customersOperations import Customer,fromString
+def end():
+    s.close()
+atexit.register(end)
+IP="127.0.0.1"
+s=socket.socket()
+port=12344
+s.connect((IP,port))
+
+tilemap = []
+
+newCustomers = []
+def read():
+    l=int(s.recv(4))
+    m=s.recv(l)
+    m=m.decode("utf-8")
+    return m
+tilemap=eval(read())
+def listen():
+    while True:
+        m=read()
+        if m.startswith("customers"):
+            m = m[9:]
+            print(m)
+            newCustomers.append(fromString(m))
+        print(m)
+t1=threading.Thread(target=listen)
+t1.start()
+
+
+def getTileMap():
+    return tilemap
+
+def getNewCustomers():
+    return newCustomers
+def sendPrices(prices):
+    s.send(prices)
